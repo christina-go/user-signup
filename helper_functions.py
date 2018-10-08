d1caaf8e7e110e41d06a9647552909e239cd1bfd
Christina Gomez <christina.gomez27@gmail.com>
2018-10-07 20:28:47 -0400

    finished user signup assignment

--- helper_functions.py
+++ helper_functions.py
@@ -22,35 +22,3 @@ def special_chars(userEmail):
     return True
   else:
     return False  
-
-
-def email_validation(userEmail):
-
-  if text_len(userEmail) and no_spaces   (userEmail) and special_chars(userEmail) ==  True:
-    return True
-  else:
-    return False
-
-def username_validation(userName):
-
-  if text_len(userName) and no_spaces(userName) == True:
-    return True
-  else:
-    return False
-
-def password_validation(password):
-
-  if text_len(password) and no_spaces(password) == True:
-    return True
-
-  else:
-    return False
-
-def password_match(password, passwordConfirmation):
-  if password == password_confirmation:
-    return True
-
-  else:
-    return False 
-    
-
--- main.py
+++ main.py
@@ -6,44 +6,6 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
-def username_validation(username):
-
-  if text_len(username) and no_spaces(username) == True:
-      
-    return True
-  else:
-
-    return False
-
-def password_validation(password):
-
-  if text_len(password) and no_spaces(password) == True:
-    return True
-
-  else:
-    return False
-
-def password_match(password, passwordConfirmation):
-    if passwordConfirmation == '':
-        return False 
-    
-    elif password == passwordConfirmation:
-        return True   
-
-    else:
-        return False 
-
-def email_validation(userEmail):
-
-  if userEmail == '':
-      return True
-  
-  elif text_len(userEmail) and no_spaces(userEmail) and special_chars(userEmail) ==  True:
-      return True
-  
-  else:
-    return False
-
 @app.route("/", methods=['GET'])
 def index():
     return render_template('signup.html')
@@ -51,11 +13,60 @@ def index():
 
 @app.route("/", methods=['POST'])
 def validate_signup():
+
+# here we are creating variables for the validation functions
+# by requesting data from the form
+        
     usernameField = request.form['username']
     passwordField = request.form['password']
     passwordConfirmationField = request.form['passwordConfirmation']
     emailField = request.form['email']
     error = "That is not a valid input"
+
+#these functions validate the user input against the 
+# standards of the assignment
+
+    def username_validation(username):
+    
+      if text_len(username) and no_spaces(username) == True:
+          
+        return True
+      else:
+      
+        return False
+    
+    def password_validation(password):
+    
+      if text_len(password) and no_spaces(password) == True:
+        return True
+    
+      else:
+        return False
+    
+    def password_match(password, passwordConfirmation):
+        if passwordConfirmation == '':
+            return False 
+        
+        elif password == passwordConfirmation:
+            return True   
+    
+        else:
+            return False 
+    
+    def email_validation(userEmail):
+    
+      if userEmail == '':
+          return True
+    
+      elif text_len(userEmail) and no_spaces(userEmail) and special_chars(userEmail) ==  True:
+          return True
+    
+      else:
+        return False    
+    
+
+# this section will trigger an error for each input field 
+# if the user input does not meet validation
 
     usernameError = " "
     passwordError = " "
@@ -73,37 +84,22 @@ def validate_signup():
 
     if not email_validation(emailField):
         emailError = error  
-  
 
+    # this redirects the user to the welcome page if all fields pass validation
     if username_validation(usernameField) is True and password_validation(passwordField) is True and password_match(passwordField, passwordConfirmationField) is True and email_validation(emailField) is True:
         return redirect('/welcome?username={0}'.format(usernameField))
-    
+
+    # this loads the data the user entered into the form after the error   
     else:        
-        #this loads the data the user entered into the form after the error
         if not username_validation(usernameField):
             return render_template('signup.html', username=usernameField,email=emailField, 
             usernameError=usernameError, passwordError=passwordError,
             passwordConfirmationError=passwordConfirmationError, emailError=emailError,)
-  
-#got the error message to appear next to one field. how
-#can i make it appear on multiple? right now the code
-#will stop at the first return. should i nest a function
-#within this one?
-
+    
 
 @app.route("/welcome")
 def welcome():
     username = request.args.get('username')
     return render_template('welcome.html', username=username)
-    #return '<h1> Welcome {0}</h1>'.format(username)
-
-
 
 app.run()    
-
-
-#validate data functions
-#def validate_data():
-#if statements return a redirect to the 
-# homepage with error as a query parameter
-#if successful, return render.template
